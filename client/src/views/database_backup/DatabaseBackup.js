f341037853f3945300797d7fd04cf9a54bec3d33
John Cris <johncrismanabo9@gmail.com>
2023-08-01 11:27:44 +0800

    manage database backup

--- client/src/views/database_backup/DatabaseBackup.js
+++ client/src/views/database_backup/DatabaseBackup.js
@@ -1,19 +1,39 @@
 import React from 'react'
-import { CButton, CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react'
+import { CButton, CCard, CCardHeader, CCol, CRow } from '@coreui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import ip from './../../constant/ip'
 
 const DatabaseBackup = () => {
+  const table = 'backup'
+  const handleBackup = () => {
+    axios
+      .get(ip + table) // Replace this with the actual URL of your Express.js server
+      .then((response) => {
+        // Trigger the download of the backup file
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        link.setAttribute('download', 'backup.sql') // Change this to the desired backup file name
+        document.body.appendChild(link)
+        link.click()
+      })
+      .catch((error) => {
+        console.error('Error backing up database:', error)
+      })
+  }
+
   return (
     <CRow>
       <CCol xs={12}>
         <CCard className="mb-4">
           <CCardHeader>
-            <strong>DatabaseBackup</strong>
+            <strong>Database Backup</strong>
+            <CButton color="primary" className="float-end" onClick={handleBackup}>
+              <FontAwesomeIcon icon={faDatabase} /> Backup
+            </CButton>
           </CCardHeader>
-          <CCardBody>
-            <h1>DatabaseBackup</h1>
-          </CCardBody>
         </CCard>
       </CCol>
     </CRow>
